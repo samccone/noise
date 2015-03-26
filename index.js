d90837fdd94268004d5968831f2fb24b0a9415b7
Sam Saccone <samccone@gmail.com>
2015-03-26 18:07:29 -0400

    Fix basic paint logic

--- index.js
+++ index.js
@@ -1,60 +1,51 @@
 var bufferFrameSize = 256;
-var context = new (window.AudioContext || window.webkitAudioContext);
+var SampleRate = 44100;
+var Audio = (window.AudioContext || window.webkitAudioContext);
+var context = new Audio();
 
-var data = '10';
+var data = '111000111';
 
 var lastTime = 0;
 var out = [];
 var ctx = document.querySelector('canvas').getContext('2d');
 var processData = [];
 var k;
+var x = 0;
 
-(function() {
-  //ctx.clearRect(0,0, 2048, 200);
+function paint(d) {
   ctx.beginPath();
 
-  for(var i = 0; i < processData.length; ++i) {
-    if (i%17 == 0) {
-      ctx.fillRect(i/2, 65, 2, 2);
-    }
-    ctx.lineTo(i/2, 100+processData[i]*20, 1, 1);
+  for(var i = 0; i < d.length; ++i) {
+    ctx.lineTo(x+i/2, 100+d[i]*20, 1, 1);
   }
+  x+= d.length/2;
 
-  ctx.stroke()
+  ctx.stroke();
   ctx.closePath();
-
-  requestAnimationFrame(arguments.callee);
-})()
+}
 
 function run() {
   var sampleRate = context.sampleRate;
   var i=0;
 
-  var baud =
-    parseInt(document.querySelector('[name="baud"]').value)
-    ||
-    100;
-  var mark = parseInt(document.querySelector('[name="mark"]').value)
-    ||
-    2000;
-  var shift = parseInt(document.querySelector('[name="shift"]').value)
-    || 1000
+  var baud = parseInt(document.querySelector('[name="baud"]').value);
+  var mark = parseInt(document.querySelector('[name="mark"]').value);
+  var shift = parseInt(document.querySelector('[name="shift"]').value);
 
-
-  k = 0.5 + (bufferFrameSize * (mark+shift) / 44100)
+  k = 0.5 + (bufferFrameSize * (mark+shift) / SampleRate);
 
   length = 1/baud;
 
   osc = context.createOscillator();
   processor = context.createScriptProcessor(bufferFrameSize, 1, 1);
   processor.onaudioprocess = function(e) {
-    console.log("tick");
     var q = [0,0,0];
     processData = e.inputBuffer.getChannelData(0);
+    paint(processData);
     for(var i =0; i< bufferFrameSize; i++) {
       var w = 2*i/bufferFrameSize;
       cos = Math.cos(w);
-      sin = Math.sin(w)
+      sin = Math.sin(w);
       coeff = 2 * cos;
       q[0] = coeff * q[1] - q[2] + processData[i];
       q[2] = q[1];
@@ -69,16 +60,16 @@ function run() {
       real: real,
       imag: imag,
       magnitude: magnitude
-    })
-  }
+    });
+  };
 
   while(i++ < data.length) {
-    osc.frequency.setValueAtTime(data[i] == '1' ? mark : mark+shift, i*length+context.currentTime);
+    osc.frequency.setValueAtTime(data[i] == '1' ? mark+shift : mark, i*length+context.currentTime);
   }
   osc.frequency.value = mark;
 
   osc.connect(processor);
-  osc.connect(context.destination)
+  osc.connect(context.destination);
   processor.connect(context.destination);
 
   osc.start();
@@ -93,6 +84,6 @@ function run() {
     osc.disconnect(processor);
     osc.disconnect(context.destination);
     processor.disconnect(context.destination);
-  }
+  };
 }
 
