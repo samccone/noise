3594f76774a549eced26372792d13430f7bf8097
Sam Saccone <samccone@gmail.com>
2015-04-02 16:17:33 -0400

    Use google style

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ var binaryToString = require('binary-to-string');
 function paint(d, m) {
   ctx.beginPath();
 
-  for(var i = 0; i < d.length; ++i) {
-    ctx.lineTo(x+i/4, 100+d[i]*20, 1, 1);
+  for (var i = 0; i < d.length; ++i) {
+    ctx.lineTo(x + i / 4, 100 + d[i] * 20, 1, 1);
   }
-  x+= d.length/4;
+  x += d.length / 4;
 
   ctx.stroke();
   ctx.closePath();
@@ -29,18 +29,18 @@ function paint(d, m) {
 }
 
 function paintAll(out, data) {
-  var sorted = out.map(function(v){return v.m;})
-  .filter(function(v){return v !== -Infinity;})
-  .sort(function(a, b){ return a - b; });
+  var sorted = out.map(function(v) { return v.m; })
+  .filter(function(v) { return v !== -Infinity; })
+  .sort(function(a, b) { return a - b; });
 
   var min = sorted[0];
-  var max = sorted[data.length-1];
+  var max = sorted[data.length - 1];
 
-  for(var i = 0; i < out.length; ++i) {
+  for (var i = 0; i < out.length; ++i) {
     ctx.fillStyle = data[i] == '0' ? 'rgba(0,0,255,0.2)' : 'rgba(255,0,0,0.2)';
-    ctx.fillRect(i*20, 0, 10, 800);
-    ctx.fillStyle = out[i].m > LIMIT ? "rgba(255,0,0,0.5)" : "rgba(0,0,255, 0.5)";
-    ctx.fillRect(i*20, out[i].m - min, 10, 10);
+    ctx.fillRect(i * 20, 0, 10, 800);
+    ctx.fillStyle = out[i].m > LIMIT ? 'rgba(255,0,0,0.5)' : 'rgba(0,0,255, 0.5)';
+    ctx.fillRect(i * 20, out[i].m - min, 10, 10);
   }
 }
 
@@ -55,15 +55,15 @@ function decode(out) {
 }
 
 function processChunk(k, binsPerBit, raw, out) {
-  while(raw.length >= binsPerBit) {
+  while (raw.length >= binsPerBit) {
     var chunk = raw.slice(0, binsPerBit);
     raw = raw.slice(binsPerBit);
-    var realW = 2 * Math.cos(2 * Math.PI * k /binsPerBit);
-    var imagW = Math.sin(2 * Math.PI*k/binsPerBit);
+    var realW = 2 * Math.cos(2 * Math.PI * k / binsPerBit);
+    var imagW = Math.sin(2 * Math.PI * k / binsPerBit);
     var d1 = 0.0;
     var d2 = 0.0;
 
-    for(var i=0; i < binsPerBit; ++i) {
+    for (var i = 0; i < binsPerBit; ++i) {
       var y = chunk[i] + realW * d1 - d2;
       d2 = d1;
       d1 = y;
@@ -75,7 +75,7 @@ function processChunk(k, binsPerBit, raw, out) {
     out.push({
       real: r,
       imag: im * d1,
-      m: 20 * Math.log(Math.sqrt(Math.pow(r,2) + Math.pow(im,2)))
+      m: 20 * Math.log(Math.sqrt(Math.pow(r, 2) + Math.pow(im, 2)))
     });
   }
 
@@ -86,12 +86,12 @@ function run() {
   var baud = parseInt(document.querySelector('[name="baud"]').value);
   var low = parseInt(document.querySelector('[name="low"]').value);
   var high = parseInt(document.querySelector('[name="high"]').value);
-  var binsPerBit = Math.ceil(SAMPLE_RATE/baud);
+  var binsPerBit = Math.ceil(SAMPLE_RATE / baud);
   data = stringToBinary(document.querySelector('[name="message"]').value);
   remainder = [];
 
   k = 0.5 + (binsPerBit * (high) / SAMPLE_RATE);
-  length = 1/baud;
+  length = 1 / baud;
 
   osc = context.createOscillator();
   osc2 = context.createOscillator();
@@ -109,8 +109,8 @@ function run() {
   };
 
   data.split('').forEach(function(v, i) {
-    osc2.frequency.setValueAtTime(Math.random()*2000, i*length+context.currentTime);
-    osc.frequency.setValueAtTime(v == '1' ? high : low, i*length+context.currentTime);
+    osc2.frequency.setValueAtTime(Math.random() * 2000, i * length + context.currentTime);
+    osc.frequency.setValueAtTime(v == '1' ? high : low, i * length + context.currentTime);
   });
 
   //osc2.start();
@@ -118,8 +118,8 @@ function run() {
   osc.frequency.value = 0;
 
   //osc2.stop(data.length*length+context.currentTime);
-  osc.stop(data.length*length+context.currentTime);
-  lastTime = data.length*length;
+  osc.stop(data.length * length + context.currentTime);
+  lastTime = data.length * length;
 
   osc.onended = function() {
     osc.disconnect(processor);
@@ -136,7 +136,7 @@ function startMic() {
     var baud = parseInt(document.querySelector('[name="baud"]').value);
     var low = parseInt(document.querySelector('[name="low"]').value);
     var high = parseInt(document.querySelector('[name="high"]').value);
-    var binsPerBit = Math.ceil(SAMPLE_RATE/baud);
+    var binsPerBit = Math.ceil(SAMPLE_RATE / baud);
     remainder = [];
     k = 0.5 + (binsPerBit * (high) / SAMPLE_RATE);
 
